f26408ecb6be66efb0609ced8387fe3080ebaa25
Elza.Sharafutdinova <elza.shar@gmail.com>
2020-07-26 15:09:19 +0400

    correct

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const entities = [
-      { link: 'Rostov-on-Don, Admiral',
-        img: 'img/image2.1.jpg',
-        dot: '0',
-        city: 'Rostov-on-Don \n LCD Admiral',
-        area: '81 m2',
-        time: '3,5 months',
-        cost: 'Upon request'
-      },
-        
-      { link: 'Sochi Thieves',
-        img: 'img/image2.2.jpg',
-        dot: '1',
-        city: 'Sochi \n Thieves',
-        area: '105 m2',
-        time: '4 months',
-        cost: 'Upon request'
-      },
+    { link: 'Rostov-on-Don, Admiral',
+    img: 'img/image2.1.jpg',
+    dot: '0',
+    city: 'Rostov-on-Don', 
+    district: 'LCD Admiral',
+    area: '81 m2',
+    time: '3,5 months',
+    cost: 'Upon request'
+  },
+    
+  { link: 'Sochi Thieves',
+    img: 'img/image2.2.jpg',
+    dot: '1',
+    city: 'Sochi', 
+    district: 'Thieves',
+    area: '105 m2',
+    time: '4 months',
+    cost: 'Upon request'
+  },
 
-      { link: 'Rostov-on-Don Patriotic',
-        img: 'img/image2.3.jpg',
-        dot: '2',
-        city: 'Rostov-on-Don \n Patriotic',
-        area: '93 m2',
-        time: '3 months',
-        cost: 'Upon request'
-      }
+  { link: 'Rostov-on-Don Patriotic',
+    img: 'img/image2.3.jpg',
+    dot: '2',
+    city: 'Rostov-on-Don', 
+    district: 'Patriotic',
+    area: '93 m2',
+    time: '3 months',
+    cost: 'Upon request'
+  }
   ];
 
 
@@ -32,6 +35,7 @@ const entities = [
         dots = document.querySelectorAll('.dot')
         img = document.querySelector('.slider_img'),
         city = document.querySelector('.city'),
+        district = document.querySelector('.district')
         area = document.querySelector('.area'),
         time = document.querySelector('.time'),
         cost = document.querySelector('.cost'),
@@ -41,6 +45,7 @@ const entities = [
 
 const setEntity = (index) => {
     city.innerText = entities[index].city;
+    district.innerText = entities[index].district;
     img.src = entities[index].img;
     area.innerText = entities[index].area;
     time.innerText = entities[index].time;
